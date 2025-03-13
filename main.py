cab3cc87b555c1a3fd22d1c0338ce9f684613fd7
bigfoodk <satcom1600@hanmail.net>
2025-03-13 17:23:55 +0900

    Remove unusing imports

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import os
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-import subprocess
-import pickle
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
